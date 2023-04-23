52b456bb41130ecc21f06cd307bb0510fe0a7326
GR <42920670+giuliorocca@users.noreply.github.com>
2023-04-23 17:59:43 +0200

    add game round info and scores to html output

--- javascript.js
+++ javascript.js
@@ -6,13 +6,13 @@ let computerWins = null;
 let playerScore = 0;
 let computerScore = 0;
 
-// Make computer randomly choose: rock, paper, or scissors
+// Make computer randomly choose from: rock, paper, scissors
 function getComputerChoice() {
     let choiceArray = ['rock', 'paper', 'scissors']
     // Generate a random number 0 > x < 1 with Math.random()
     // Multiply by the array length using the .length method on the array
     // Round down to nearest integer with Math.floor()
-    // Return random integer from [0, 1, 2], which corresponds to the index value of the array  
+    // Returns random integer from [0, 1, 2], which corresponds to the index value of the array  
     let computerChoice = Math.floor((Math.random()*choiceArray.length));
     if (computerChoice === 0) {
         computerChoice = choiceArray[0]; 
@@ -24,18 +24,10 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-// Make player choose: rock, paper, or scissors
-//function getPlayerChoice() {
-//    let playerChoice = prompt ('To play, type: rock, paper, or scissors');
-//    playerChoice = playerChoice.toLowerCase();
-//    return playerChoice;
-//}
-
-// Determine win or loss each round based on outcomes table
+// Determine winner each round based on outcomes table
 function playRound (playerChoice, computerChoice) {
 
     switch(playerChoice) {
-
         // Determine who wins when player picks rock
         case 'rock':
             if (computerChoice === 'scissors') {
@@ -95,48 +87,69 @@ function playRound (playerChoice, computerChoice) {
                 computerWins = false;
                 return ('You win. Scissors beats paper.');
             }
-
     }
 }
-    
-// Play one round of the game and keep score
-function playGame (playerSelection, computerSelection) {
+  
+// Initialize new paragraph element that will show the player/computer
+const choicesdiv = document.getElementById('choicesdiv');
+const choicesdivcontent = document.createElement('p');
 
-        playRound(playerSelection, computerSelection);
-        
-        if (playerWins === true) {
-            playerScore++;
-        } 
-        if (computerWins === true) {
-            computerScore++;
-        }
-        
-        console.log(playerSelection, computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(playerScore, computerScore);
-    }
+// Initialize new paragraph element that will show the round outcome
+const outcomediv = document.getElementById('outcomediv');
+const outcomedivcontent = document.createElement('p');
 
-// Functions passed in button EventListeners that correspond to player choices
+// Initialize new paragraph element that will show the score
+const scorediv = document.getElementById('scorediv');
+const scoredivcontent = document.createElement('p');
+
+// Create functions that are passed into EventListeners of buttons
 function playerRock () {
     playGame('rock', getComputerChoice());
 }
-
 function playerPaper () {
     playGame('paper', getComputerChoice());
 }
-
 function playerScissors () {
     playGame('scissors', getComputerChoice());
 }
 
-//Player presses 'rock' button
+//Player clicks 'rock' button
 const rockButton = document.getElementById('rockButton');
 rockButton.addEventListener('click', playerRock);
 
-//Player presses 'paper' button
+//Player clicks 'paper' button
 const paperButton = document.getElementById('paperButton');
 paperButton.addEventListener('click', playerPaper);
 
-//Player presses 'scissors' button
+//Player clicks'scissors' button
 const scissorsButton = document.getElementById('scissorsButton');
 scissorsButton.addEventListener('click', playerScissors);
+
+// Play one round of the game and keep score
+function playGame (playerSelection, computerSelection) {
+        
+    if (playerWins === true) {
+        playerScore++;
+    } 
+    if (computerWins === true) {
+        computerScore++;
+    }
+    if (playerScore || computerScore === 5 && playerScore > computerScore) {
+            alert('You win')
+    }
+    if (playerScore || computerScore === 5 && playerScore < computerScore) {
+            alert('You lose')
+    }    
+}
+
+// Add text showing the player/computer choices in the newly created paragraph element
+choicesdivcontent.textContent = `You played ${playerSelection} and computer played ${computerSelection}`;
+choicesdiv.appendChild(choicesdivcontent);
+
+// Add text showing the round outcome in the newly created paragraph element
+outcomedivcontent.textContent = playRound(playerSelection, computerSelection);
+outcomediv.appendChild(outcomedivcontent);
+        
+// Add text showing score in newly created paragraph element
+scoredivcontent.textContent = `Player Score ${playerScore} Computer Score ${computerScore}`;
+scorediv.appendChild(scoredivcontent);
